1aba5cf36d6ad5282c3b52405e1d67c76d0f2f25
Nishant Kashyap <31220060+Ndroid21@users.noreply.github.com>
2026-07-17 22:27:20 +0530

    Time: 0 ms (100%), Space: 59.3 MB (26.56%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.js
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.js
@@ -9,34 +9,21 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var oddEvenList = function(head) {
-    if(!head || !head.next) return head;
+var oddEvenList = function (head) {
+    if (!head || !head.next) return head;
 
     let odd = head, even = head.next;
     let evenHead = even;
 
-    while((odd && odd.next) && (even && even.next)) {
+    while (odd.next && even.next) {
         odd.next = odd.next.next;
         even.next = even.next.next;
 
-        if(odd.next)
-            odd = odd.next;
+        odd = odd.next;
         even = even.next;
     }
 
-    while(odd && odd.next) {
-        odd.next = odd.next.next;
-        if(odd.next)
-            odd = odd.next;
-    }
-
-    while(even && even.next) {
-        even.next = even.next.next;
-        even = even.next;
-    }
-
-    if(odd)
-        odd.next = evenHead;
+    odd.next = evenHead;
 
     return head;
 };
