d3ff2507d69e8675dec922683ef37896e5ee2f4b
Nishant Kashyap <31220060+Ndroid21@users.noreply.github.com>
2026-07-18 18:10:29 +0530

    Time: 3 ms (54.82%), Space: 61.5 MB (59.81%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -11,65 +11,21 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-    let curr1 = l1, curr2 = l2, carry = 0;
+    let carry = 0;
+    let l3 = new ListNode();
+    let ans = l3;
 
-    let newList = new MyLinkedList();
+    while(l1 || l2 || carry) {
+        let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+        let digit = sum % 10;
+        carry = Math.floor(sum/10);
 
-    while (curr1 && curr2) {
-        let sum = curr1.val + curr2.val + carry;
-        let rem = sum % 10;
-        carry = Math.floor(sum / 10);
-
-        newList.add(rem);
-        curr1 = curr1.next;
-        curr2 = curr2.next;
+        let newNode = new ListNode(digit);
+        l3.next = newNode;
+        l3 = l3.next;
+        l2 = l2 && l2.next;
+        l1 = l1 && l1.next;
     }
 
-    while (curr1) {
-        let sum = curr1.val + carry;
-        let rem = sum % 10;
-        carry = Math.floor(sum / 10);
-
-        newList.add(rem);
-        curr1 = curr1.next;
-    }
-
-    while (curr2) {
-        let sum = curr2.val + carry;
-        let rem = sum % 10;
-        carry = Math.floor(sum / 10);
-
-        newList.add(rem);
-        curr2 = curr2.next;
-    }
-
-    if (carry) {
-        newList.add(carry);
-    }
-
-    return newList.getHead();
+    return ans.next;
 };
-
-class MyLinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-    }
-
-    add(val) {
-        const newNode = new ListNode(val);
-
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = this.head;
-            return;
-        }
-
-        this.tail.next = newNode;
-        this.tail = newNode;
-    }
-
-    getHead() {
-        return this.head;
-    }
-}
