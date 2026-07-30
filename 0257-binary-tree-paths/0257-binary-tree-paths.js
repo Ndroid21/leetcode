15ada19b85a89644f8aa2262a8e87f25c69a7ea1
Nishant Kashyap <31220060+Ndroid21@users.noreply.github.com>
2026-07-30 22:15:50 +0530

    Time: 0 ms (100%), Space: 55.3 MB (61.35%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+    const dfs = function (node) {
+        if(!node.left && !node.right) {
+            path.push(node.val);
+            result.push(path.join('->'));
+            path.pop();
+            return;
+        }
+
+        path.push(node.val);
+        node.left && dfs(node.left);
+        node.right && dfs(node.right);
+        path.pop();
+    }
+
+    const path = [];
+    const result = [];
+    dfs(root);
+
+    return result;
+};
