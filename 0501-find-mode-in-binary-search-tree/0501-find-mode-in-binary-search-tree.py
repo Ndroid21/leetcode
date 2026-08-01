ee3f84619fb1c60ccb2e5798004b8558e63af8a5
Nishant Kashyap <31220060+Ndroid21@users.noreply.github.com>
2026-08-02 00:48:06 +0530

    Time: 2 ms (79.93%), Space: 22 MB (82.78%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -6,30 +6,34 @@
 #         self.right = right
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
-        modeCount = 0
-        map = {}
-        def dfs (node):
-            nonlocal modeCount
+        prev = None
+        count = 0
+        max = 0
+        result = []
+
+        def dfs(node):
+            nonlocal prev, count, max, result
+
             if node is None:
                 return
-            
-            if node.val not in map:
-                map[node.val] = 1
-            else:
-                map[node.val] = map[node.val] + 1
-
-            if modeCount < map[node.val]:
-                modeCount = map[node.val]
 
             dfs(node.left)
+
+            if prev is not None and prev.val == node.val:
+                count += 1
+            else:
+                count = 1
+
+            if max < count:
+                result = [node.val]
+                max = count
+            elif max == count:
+                result.append(node.val)
+
+            prev = node
+
             dfs(node.right)
-        
 
         dfs(root)
 
-        result = []
-        for key, val in map.items():
-            if val == modeCount:
-                result.append(key)
-        
         return result
