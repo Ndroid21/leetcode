a9f26d3206d122c6b2b942ef26d225a93995b809
Nishant Kashyap <31220060+Ndroid21@users.noreply.github.com>
2026-08-02 20:56:01 +0530

    Time: 7 ms (64.6%), Space: 65.1 MB (50.15%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var islandPerimeter = function (grid) {
+    const m = grid.length;
+    const n = grid[0].length;
+    let perimeter = 0;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 1) {
+                perimeter += 4;
+
+                if (i < m - 1 && grid[i + 1][j] === 1) {
+                    perimeter -= 2;
+                }
+
+                if (j < n - 1 && grid[i][j + 1] === 1) {
+                    perimeter -= 2;
+                }
+            }
+        }
+    }
+
+    return perimeter;
+};
