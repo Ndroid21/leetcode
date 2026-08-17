f954482a7478d79b4987b36edeb29fedd720d413
Nishant Kashyap <31220060+Ndroid21@users.noreply.github.com>
2026-08-17 09:25:31 +0530

    Time: 683 ms (56.71%), Space: 199.8 MB (36.13%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
@@ -20,38 +20,22 @@ var validPath = function (n, edges, source, destination) {
         }
     }
 
-    const getNeighbors = function (node) {
-        return adjList[node];
-    }
+    const dfs = function(curr) {
+        if(curr === destination) return true;
+        visited.add(curr);
 
-    const bfs = function (root, target) {
-        const q = [root];
-        const visited = new Set();
-        visited.add(root);
+        for(let neighbor of adjList[curr]) {
+            if(visited.has(neighbor)) continue;
 
-        while (q.length > 0) {
-            const n = q.length;
-
-            for (let i = 0; i < n; i++) {
-                const curr = q.shift();
-
-                if (curr === target) return true;
-
-                for (const neighbor of getNeighbors(curr)) {
-                    if (visited.has(neighbor)) continue;
-
-                    q.push(neighbor);
-                    visited.add(neighbor);
-                }
-
-            }
+            if(dfs(neighbor)) return true;
         }
 
         return false;
     }
 
+    const visited = new Set();
     const adjList = new Map();
     generateAdjList(adjList);
 
-    return bfs(source, destination);
+    return dfs(source);
 };
