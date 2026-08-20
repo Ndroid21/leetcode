6559c86443b98c746b029c43d002bbcc37f4c891
Nishant Kashyap <31220060+Ndroid21@users.noreply.github.com>
2026-08-20 09:24:56 +0530

    Time: 835 ms (24.28%), Space: 19.4 MB (51.81%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -1,17 +1,19 @@
 class Solution:
     def countArrangement(self, n: int) -> int:
-        result = []
+        visited = [False for _ in range(n + 1)]
+        count = 0
 
-        def dfs(path):
-            if len(path) == n:
-                result.append(path[:])
+        def dfs(index):
+            nonlocal count
+            if index == n + 1:
+                count += 1
                 return
 
             for perm in range(1, n + 1):
-                if perm not in path and isArrangementBeautiful(perm, len(path) + 1):
-                    path.append(perm)
-                    dfs(path)
-                    path.pop()
+                if visited[perm] == False and isArrangementBeautiful(perm, index):
+                    visited[perm] = True
+                    dfs(index + 1)
+                    visited[perm] = False
 
         def isArrangementBeautiful(arrangement, i):
             if arrangement % i == 0 or i % arrangement == 0:
@@ -19,6 +21,6 @@ class Solution:
 
             return False
 
-        dfs([])
+        dfs(1)
 
-        return len(result)
+        return count
