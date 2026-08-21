226f9f41d652507812b3948342a41e7c4ba74d97
Nishant Kashyap <31220060+Ndroid21@users.noreply.github.com>
2026-08-21 07:02:12 +0530

    Time: 2 ms (50%), Space: 56.7 MB (84.38%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.js
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.js
@@ -3,15 +3,11 @@
  * @return {number[]}
  */
 var resultArray = function (nums) {
-    const arr1 = [];
-    arr1.push(nums[0]);
-    const arr2 = [];
-    arr2.push(nums[1]);
+    const arr1 = [nums[0]];
+    const arr2 = [nums[1]];
 
     for (let i = 2; i < nums.length; i++) {
-        let arr1Last = arr1[arr1.length - 1];
-        let arr2Last = arr2[arr2.length - 1];
-        if (arr1Last > arr2Last) {
+        if (arr1[arr1.length - 1] > arr2[arr2.length - 1]) {
             arr1.push(nums[i]);
         } else {
             arr2.push(nums[i]);
