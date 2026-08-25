d453312cea9a13df8d3139706a8a0fd629311e07
Nishant Kashyap <31220060+Ndroid21@users.noreply.github.com>
2026-08-25 20:41:14 +0530

    Time: 1 ms (61.76%), Space: 55.4 MB (82.35%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.js
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.js
@@ -4,18 +4,14 @@
  * @return {number}
  */
 var missingMultiple = function(arr, k) {
-    let nums = new Set();
-
-    for(let i=0; i<arr.length; i++) {
-        nums.add(arr[i]);
-    }
+    let nums = new Set(arr);
 
     let i = 1;
     let missing = -1;
 
     while(true) {
-        if(!nums.has(k * i)) {
-            missing = k * i;
+        missing = k * i;
+        if(!nums.has(missing)) {
             break;
         }
 
