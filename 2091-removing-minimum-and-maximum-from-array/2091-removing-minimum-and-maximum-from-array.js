a46c856b66a57be246afbb88904c7273ed4642b5
Nishant Kashyap <31220060+Ndroid21@users.noreply.github.com>
2026-08-30 15:50:55 +0530

    Time: 0 ms (100%), Space: 66.9 MB (89.66%) - LeetHub

---
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumDeletions = function (nums) {
+    const n = nums.length;
+    let minIdx = 0;
+    let maxIdx = 0;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[minIdx] > nums[i]) {
+            minIdx = i;
+        }
+
+        if (nums[maxIdx] < nums[i]) {
+            maxIdx = i;
+        }
+    }
+
+    const fromLeft = Math.max(minIdx, maxIdx) + 1;
+    const fromRight = n - Math.min(minIdx, maxIdx);
+    const fromBoth = (Math.min(minIdx, maxIdx) + 1) + (n - Math.max(minIdx, maxIdx));
+
+    return Math.min(fromLeft, fromRight, fromBoth);
+};
