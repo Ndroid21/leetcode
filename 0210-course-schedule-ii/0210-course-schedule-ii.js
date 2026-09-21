073e9872d1e19314f83bd7609318ca7b3650ae13
Nishant Kashyap <31220060+Ndroid21@users.noreply.github.com>
2026-09-21 12:48:33 +0530

    Time: 22 ms (9.06%), Space: 64.2 MB (9.79%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.js
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -13,9 +13,7 @@ var findOrder = function (numCourses, prerequisites) {
     }
 
     for (let [to, from] of prerequisites) {
-        const list = adjList.get(from);
-        list.push(to);
-        adjList.set(from, list);
+        adjList.get(from).push(to);
         inDegree.set(to, inDegree.get(to) + 1);
     }
 
@@ -26,20 +24,16 @@ var findOrder = function (numCourses, prerequisites) {
         }
     }
 
-    let count = 0;
     const result = [];
     while (q.length > 0) {
         const node = q.shift();
         result.push(node);
-        count++;
 
         for (const neighbor of adjList.get(node)) {
             inDegree.set(neighbor, inDegree.get(neighbor) - 1);
-
             if (inDegree.get(neighbor) === 0) q.push(neighbor);
-
         }
     }
 
-    return count === numCourses ? result : [];
+    return result.length === numCourses ? result : [];
 };
